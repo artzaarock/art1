627a02b536de79def10a7ea13bcd8e8e8dd873d4
artzaarock <73689671+artzaarock@users.noreply.github.com>
2020-11-06 19:55:38 +0700

    Add files via upload

---
+++ index.html.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hello</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <div class="card">
+                    <img class="card-img-top" src="cat11.jpg">
+                    <div class="card-body">
+                        <h4class="card-title">Abyssinian</h4>
+                        <p class="card-text">The Abyssinian is often a colorful cat with a distinctly ticked coat, medium in size and regal in appearance; lithe, hard and muscular, showing eager activity and lively interest in their surroundings.
+                        </p>
+                        <a href="#" class="btn btn-primary">Read more</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card">
+                    <img class="card-img-top" src="cat22.jpg">
+                    <div class="card-body">
+                        <h4class="card-title">American Bobtail</h4>
+                        <p class="card-text">The American Bobtail is generally medium to large cat, with a naturally occurring bobbed tail. The American Bobtail is athletic and usually well-muscled with a sometimes powerful look. </p>
+                        <a href="#" class="btn btn-primary">Read more</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card">
+                    <img class="card-img-top" src="cat33.jpg">
+                    <div class="card-body">
+                        <h4class="card-title">American Curl</h4>
+                        <p class="card-text">The distinctive feature of the American Curl is their attractive, uniquely curled-back ears. Elegant, well balanced, moderately muscled, slender rather than massive in build.</p>
+                        <a href="#" class="btn btn-primary">Read more</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ index1.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>LAB-ITF</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3">
+                <img src="cat11.jpg" style="height:300px;" style="width: 100px;"class="img-fluid rounded" />
+            </div>
+            <div class="col-sm-3">
+                <div class="col-md-6">
+                    <img src="cat22.jpg" style="height:100px;" style="width: 100px;"class="img-fluid img-thumbnail" />
+                    <img src="cat33.jpg" style="height:100px;" style="width: 100px;"class="img-fluid img-thumbnail" />
+                    <img src="cat44.jpg" style="height:100px;" style="width: 100px;"class="img-fluid img-thumbnail" />
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <img src="cat55.jpg" style="height: 100px;" style="width: 50px;" class="img-fluid" />
+                <img src="cat66.jpg" style="height: 100px;" style="width: 50px;" class="img-fluid img-thumbnail" />
+            </div>
+        </div> <!-- Close Row -->
+    </div> <!-- Close container -->
+</body>
+</html>
